a38b70c536b574a6ae23d334cbe9b08d300cbe4a
joaovic499 <joaovictorsilva60@Live.com>
2024-05-26 01:26:31 -0300

    metodo put e delete finalizados

--- src/index.js
+++ src/index.js
@@ -27,8 +27,43 @@ app.put('/jogadores/:id', function(req, res) {
     const {id} = req.params
     const {nome, posicao, overall} = req.body
 
-    return response.json()
+    const jogadorIndex = jogadores.findIndex(p => p.id === id)
+
+    if (jogadorIndex < 0 ) {
+        return res.status(404).json({error: "Jogador não encontrado"})
+    }
+
+    if (!nome || !posicao  || !overall ) {
+        return res.status(400).json({error: "Não foi possivel editar o jogador"})
+    }
+
+    const jogador = {
+        id,
+        nome,
+        posicao,
+        overall
+    }
+
+    jogadores[jogadorIndex] = jogador
+    
+    return res.json(jogador)
 })
+
+app.delete('/jogadores/:id', function(req, res) {
+    const {id} = req.params
+
+    const jogadorIndex = jogadores.findIndex(p => p.id === id)
+
+    if(jogadorIndex < 0) {
+        return res.status(404).json({error: 'Jogador  nao encontrado'})
+    }
+
+    jogadores.splice(jogadorIndex, 1)
+
+    return res.status(200).json({message: 'Jogador excluido com sucesso'})
+
+})
+
 app.listen(3000, () => {
     console.log("Server rodando na porta 3000!")
 })
