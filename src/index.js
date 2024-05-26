894f37448930e471435199bbf7d3f4a63c5725df
joaovic499 <joaovictorsilva60@Live.com>
2024-05-26 00:55:51 -0300

    adiçaõ do gerador de id, e implementação do post, e get da api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+const { v4: uuidv4 } = require('uuid')
 
-app.get('/', function(req, res) {
-    return res.json({
-        message: "Olá dev seja bem vindo"
-    })
+const jogadores = []
+
+app.get('/jogadores', function(req, res) {
+    return res.json(jogadores)
+});
+
+app.post('/jogadores', function(req, res) {
+   const {nome, posicao, overall} = req.body
+   const jogador = {
+    id: uuidv4(),
+    nome,
+    posicao,
+    overall
+   }
+
+   jogadores.push(jogador)
+
+   return res.status(201).json(jogador)
 })
 
+app.put('/jogadores/:id', function(req, res) {
+    const {id} = req.params
+    const {nome, posicao, overall} = req.body
+
+    return response.json()
+})
 app.listen(3000, () => {
     console.log("Server rodando na porta 3000!")
 })
